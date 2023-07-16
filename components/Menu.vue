37f302d22338d9721c7e16319ab28c04dfcf0955
Vinicius Frota <viniciuspxfrota@gmail.com>
2023-07-15 22:25:55 -0300

    add game and menu components

---
+++ components/Menu.vue
@@ -0,0 +1,57 @@
+<template>
+    <menu>
+      <h1>Galaxy Raiders</h1>
+      <nav>
+        <button>Start Game</button>
+        <button>Leader Board</button>
+        <button>Exit</button>
+      </nav>
+    </menu>
+</template>
+  
+<style scoped>
+menu {
+  position: absolute;
+
+  height: inherit;
+  width: inherit;
+
+  background: url('~/assets/space.gif') center / cover no-repeat;
+  
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  z-index: 10;
+}
+
+h1 {
+  color: white;
+  font-size: 4vw;
+  text-transform: uppercase;
+}
+
+
+nav {
+  height: 50vh;
+  width: 30vw;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+
+  box-sizing: border-box;
+  padding: 10vh 0.5vh;
+}
+
+nav button {
+  height: 20%;
+  width: 60%;
+
+  font-size: 1.4vw;
+
+  text-transform: uppercase;
+}
+</style>
