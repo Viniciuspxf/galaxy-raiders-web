cf58ce6a897eff6e63083229c0f1b90977b41e37
Vinicius Frota <viniciuspxfrota@gmail.com>
2023-07-22 22:20:53 -0300

    finish leaderboard page

--- pages/leaderboard.vue
+++ pages/leaderboard.vue
@@ -2,6 +2,11 @@
     <section>
         <h2>Leaderboard</h2>
         <table>
+            <tr> 
+              <th>Start Time</th>
+              <th>Points</th>
+              <th>Number Of Destroyed Asteroids</th>
+            </tr>
             <tr v-for="round in leaderboard" :key="round.startTime">
                 <td> {{ round.startTime }} </td>
                 <td> {{ round.points }} </td> 
@@ -14,47 +19,59 @@
 <script>
 export default {
   async setup() {
-    const { data: leaderboard, pending: pending} = await $get("/space-field/leaderboard");
-    return { leaderboard, pending }
+    const { data: leaderboard } = await $get("/space-field/leaderboard");
+    return { leaderboard }
   },
   beforeCreate() {
     this.leaderboard.forEach(element => {
         element.startTime = new Date(element.startTime).toString();
+        element.points = Number.parseFloat(element.points).toFixed(2);
     });
-  },
-  methods: {
-    async sendPauseCommand() {
-      console.log("call command")
-      await $post("/ship/commands", { command: "PAUSE_GAME" });
-      this.isPaused = !this.isPaused;
-    },
   }
 }
 </script>
 
 
 <style scoped>
+* {
+  color: white;
+}
+
 section {
-  position: absolute;
+  height: calc(100vh - 4rem);
+  width: calc(100vw - 4rem);
 
-  height: inherit;
-  width: inherit;
+  padding: 2rem;
+  overflow: hidden;
 
-  background: url('~/assets/space.gif') center / cover no-repeat;
-  
+  position: relative;
+
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
+  
+  background: url('~/assets/space.gif') center / cover no-repeat;
 }
 
 h2 {
-  color: white;
+  margin-top: 10vh;
+  margin-bottom: 15vh;
   font-size: 4vw;
   text-transform: uppercase;
 }
 
-td {
-    color: white;
+table {
+  width: 60vw;
 }
+
+td, th {
+  font-size: 1vw;
+  text-align: center;
+}
+
+th {
+  margin-bottom: 10px;
+}
+
 </style>
